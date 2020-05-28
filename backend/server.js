c5af3b5029a4688d83b554a43e55e87ad57459c7
Emily <haw.emily@gmail.com>
2020-05-28 23:56:05 +0100

    resolve pathnamee of static assets

--- backend/server.js
+++ backend/server.js
@@ -28,7 +28,7 @@ app.get("/", (req, res) => {
 
 //Serve static assets if in production
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+  app.use(express.static("../client/build"));
 
   app.get("*", (req, res) => {
     res.sendFile(path.resolve("..", "client", "build", "index.html"));
