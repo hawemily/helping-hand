a54f12b6f771775d2a9f2971eba74601ca27b059
Charlie Victor <charlievictor@Charlies-MBP.broadband>
2020-06-04 02:03:56 +0100

    [cv218]: Altered spacing to allow for varied window sizes in response to user feedback on ipad

--- client/src/components/personInNeed/pinServiceOptions.js
+++ client/src/components/personInNeed/pinServiceOptions.js
@@ -2,40 +2,42 @@ import React from "react";
 import { Card, CardImg, CardBody, Button, Row, Col } from 'reactstrap';
 
 const Services = (props) => {
+  const optionWidth = '302px'
+
   return (
     <Row>
-      <Col sm="3">
+      <div style={{marginLeft: '5%', marginBottom: '15px', width: optionWidth}}>
         <Card>
           <CardImg top width="100%" src="./assets/temp.png" alt="Card image cap" />
           <CardBody>
           	<Button outline color="primary" block>Groceries</Button>
           </CardBody>
         </Card>
-      </Col>
-      <Col sm="3">
+      </div>  
+      <div style={{marginLeft: '2%', marginBottom: '15px', width: optionWidth}}>
         <Card>
           <CardImg top width="100%" src="../../assets/temp.png" alt="Card image cap" />
           <CardBody>
           	<Button outline color="primary" block>Laundry</Button>
           </CardBody>	
         </Card>
-      </Col>
-      <Col sm="3">
+      </div>
+      <div style={{marginLeft: '2%', marginBottom: '15px', width: optionWidth}}>
         <Card>
           <CardImg top width="100%" src="../../assets/temp.png" alt="Card image cap" />
           <CardBody>
           	<Button outline color="primary" block>Transport</Button>
           </CardBody>	
         </Card>
-      </Col>
-      <Col sm="3">
+      </div>
+      <div style={{marginLeft: '2%', marginBottom: '15px', width: optionWidth}}>
         <Card>
           <CardImg top width="100%" src="../../assets/temp.png" alt="Card image cap" />
           <CardBody>
           	<Button outline color="primary" block>Healthcare</Button>
           </CardBody>	
         </Card>
-      </Col>
+      </div>
     </Row>
   );
 };
