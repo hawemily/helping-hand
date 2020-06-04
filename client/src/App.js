73363ac36a183e358e11aedc80fa0d115daaf5de
dhivya3818 <dr3818@ic.ac.uk>
2020-06-04 12:24:47 +0100

    Modify task list to make action buttons more clear for simpler UI. Changed to use standard colors and props for TaskRequestList.

--- client/src/App.js
+++ client/src/App.js
@@ -8,27 +8,30 @@ import VolunteerTaskCard from "./components/volunteers/VolunteerTaskCard";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "react-datepicker/dist/react-datepicker.css";
 import "./main.scss";
+import TaskRequestList from "./components/general/TaskRequestList";
 
 function App() {
 
-  var sampleTask = {
-    area: 'South Kensington',
-    distance: '2.2',
-    date: '27/5/2020',
-    category: 'Groceries',
-    description: 'Bananas x5\nMilk (Semi-skimmed) 150ml\nOrange Juice 500ml',
-    expand: () => console.log('click to expand')
-  }
+    var sampleTask = {
+        id: '2284',
+        area: 'South Kensington',
+        distance: '2.2',
+        date: '27/5/2020',
+        category: 'Groceries',
+        description: 'Bananas x5\nMilk (Semi-skimmed) 150ml\nOrange Juice 500ml',
+        expand: () => console.log('click to expand')
+    }
 
-  return (
-    <div className='App'>
-      <NavBar />
-      <Volunteer />
-      <GroceryForm />
-      <Homepage />
-      <VolunteerTaskCard task={sampleTask}/>
-    </div>
-  );
+    return (
+        <div className='App'>
+            <NavBar/>
+            <Volunteer/>
+            <TaskRequestList task={sampleTask}/>
+            <GroceryForm/>
+            <Homepage/>
+            <VolunteerTaskCard task={sampleTask}/>
+        </div>
+    );
 }
 
 export default App;
--- client/src/components/general/TaskRequestList.js
+++ client/src/components/general/TaskRequestList.js
@@ -4,6 +4,9 @@ import TaskItem from "./TaskItem";
 
 
 const TaskRequestList = (props) => {
+    var task = props.task;
+    // props passed into task request list should have all the information shown,
+    // as well as information on the volunteer who confirmed it
 
     return (
         <Container variant='flush'>
@@ -22,34 +25,24 @@ const TaskRequestList = (props) => {
                         <Nav.Item>
                             <Nav.Link href="#first"> Volunteer </Nav.Link>
                         </Nav.Item>
-                        <Nav.Item>
-                            <Nav.Link href="#first"> Actions </Nav.Link>
-                        </Nav.Item>
                     </Nav>
                 </Card.Header>
                 <Card.Body>
-                    <Card border= "primary">
+                    <Card border='success' className='taskEntry'>
                         <Nav fill justify>
-                            <Nav.Item>
-                                12/6/2020
-                            </Nav.Item>
-                            <Nav.Item>
-                                #2870
-                            </Nav.Item>
-                            <Nav.Item>
-                                Groceries
-                            </Nav.Item>
-                            <Nav.Item>
-                                Bobby
-                            </Nav.Item>
-                            <Nav.Item>
-                                <Button variant='success' size='sm' block> View Details </Button>
-                                <Button variant='warning' size='sm' block> Volunteer Info </Button>
-                                <Button variant='danger' size='sm' block> Report an Issue </Button>
-                            </Nav.Item>
+                            <Nav.Item> <h5> {task.date} </h5></Nav.Item>
+                            <Nav.Item> <h5> #{task.id} </h5></Nav.Item>
+                            <Nav.Item><h5> {task.category} </h5> </Nav.Item>
+                            <Nav.Item><h5>{task.volunteerId}</h5></Nav.Item>
                         </Nav>
+                        <div className="ActionGrp">
+                            <Button className="ActionDetailsBtn">View Details</Button>
+                            <Button className="ActionInfoBtn">Volunteer Info</Button>
+                            <Button className="ActionReportBtn"> Report an Issue </Button>
+                        </div>
                     </Card>
                 </Card.Body>
+
             </Card>
         </Container>
     );
