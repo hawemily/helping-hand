70cd774bffeb59205e4b364052b2b5efcfb8eee5
yapjinxian <yapjinxian@gmail.com>
2020-06-10 19:08:25 +0800

    added checkboxes to grocery list for volunteers

--- client/src/components/volunteers/ViewOnlyBasket.js
+++ client/src/components/volunteers/ViewOnlyBasket.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Card, Button, Modal, Table } from "react-bootstrap";
+import { Card, Button, Modal, Table, Form } from "react-bootstrap";
 
 const ViewOnlyBasket = (props) => {
   var basket = [];
@@ -16,6 +16,7 @@ const ViewOnlyBasket = (props) => {
           <th>No.</th>
           <th>Item</th>
           <th>Quantity</th>
+          <th></th>
         </tr>
       </thead>
       <tbody>
@@ -25,6 +26,11 @@ const ViewOnlyBasket = (props) => {
               <td>{i + 1}</td>
               <td>{element.item}</td>
               <td>{element.quantity}</td>
+              <td>
+                <Form.Group controlId={"checkbox" + i}>
+                  <Form.Check type="checkbox" />
+                </Form.Group>
+              </td>
             </tr>
           );
         })}
