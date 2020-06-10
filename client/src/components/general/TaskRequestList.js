83e1e2f1c6cd77a557e8092069ae4cb068ade304
chris <chonghern@gmail.com>
2020-06-10 11:27:04 +0800

    fixed typeerror by rendering only when buttonmodalshow.length matches tasks.length

--- client/src/components/general/TaskRequestList.js
+++ client/src/components/general/TaskRequestList.js
@@ -29,27 +29,21 @@ const TaskRequestList = (props) => {
   console.log("PROPS")
   console.log(tasks);
 
-  // props passed into task request list should have all the information shown,
-  // as well as information on the volunteer who confirmed it
-  // THERE IS A BUG where modal only presents first elem of list
-  // TODO: try componentwillupdateprops alternative
-
-  const initialArray = Array(10).fill(0);
-  console.log("INITIALARRAY");
-  console.log(initialArray);
-
   const arr = [];
-  // const [buttonModalShow, setButtonModalShow] = useState(
-  //   Array(tasks.length).fill(defaultState)
-  // );
-  const buttonModalShow = Array(tasks.length).fill(defaultState)
+  // Initialise buttonModalShow
+  const [buttonModalShow, setButtonModalShow] = useState(
+    Array(tasks.length).fill(defaultState)
+  );
 
+  console.log("tasks length:" + tasks.length)
+  console.log("buttonmodalshow length:" + buttonModalShow.length)
+  if (tasks.length != buttonModalShow.length) {
+    // update buttonModalShow when tasks updates (backend finishes fetching)
+    console.log("update button modal show!")
+    setButtonModalShow(Array(tasks.length).fill(defaultState))
+  }
   console.log("button modals");
   console.log(buttonModalShow);
-
-  // console.log(tasks.length);
-  // console.log(Array(tasks.length).fill(defaultState));
-  // console.log(buttonModalShow);
 
   const toggleModal = (modalType, index) => {
     console.log(`modaltype: ${modalType}`);
@@ -65,102 +59,105 @@ const TaskRequestList = (props) => {
       }
       return button;
     });
+    console.log("new states")
     console.log(newStates);
-    // setButtonModalShow(newStates);
-    buttonModalShow = newStates
+    setButtonModalShow(newStates);
+    // buttonModalShow = newStates
   };
-
-  return (
-    <Container variant='flush'>
-      <Table>
-        <thead>
-          <tr className='text-center'>
-            <th>Date</th>
-            <th>Request No.</th>
-            <th>Service</th>
-            <th> Actions </th>
-            <th>Status</th>
-          </tr>
-        </thead>
-        <tbody>
-          {tasks.length !== 0 &&
-            tasks.map((task, index) => {
-              const {
-                date,
-                time,
-                taskId,
-                category,
-                volunteerId,
-                description,
-              } = task;
-              return (
-                <tr
-                  className={`text-center ${
-                    task.isCompleted
-                      ? "completed"
-                      : task.volunteerId == null
-                      ? "pending"
-                      : "confirmed"
-                  }`}
-                  key={taskId}
-                >
-                  <td className='align-middle'>
-                    {formatter.formatDate(date)}
-                    {formatter.formatTime(time)}
-                  </td>
-                  <td className='align-middle'>#{taskId}</td>
-                  <td className='align-middle'>{`${category}`}</td>
-                  <td className='align-middle'>
-                    <ButtonGroup>
-                      <Button
-                        variant='primary'
-                        onClick={() => toggleModal("view", index)}
-                      >
-                        More Details
-                      </Button>
-                      <div id={taskId}>
-                        <ViewDetailsModal
-                          show={buttonModalShow[index].view}
+  // Do not render until buttonModalShow is updated to match tasks, otherwise will throw error
+  if (tasks.length == buttonModalShow.length)
+    return (
+      <Container variant='flush'>
+        <Table>
+          <thead>
+            <tr className='text-center'>
+              <th>Date</th>
+              <th>Request No.</th>
+              <th>Service</th>
+              <th> Actions </th>
+              <th>Status</th>
+            </tr>
+          </thead>
+          <tbody>
+            {tasks.length !== 0 &&
+              tasks.map((task, index) => {
+                const {
+                  date,
+                  time,
+                  taskId,
+                  category,
+                  volunteerId,
+                  description,
+                } = task;
+                return (
+                  <tr
+                    className={`text-center ${
+                      task.isCompleted
+                        ? "completed"
+                        : task.volunteerId == null
+                        ? "pending"
+                        : "confirmed"
+                    }`}
+                    key={taskId}
+                  >
+                    <td className='align-middle'>
+                      {formatter.formatDate(date)}
+                      {formatter.formatTime(time)}
+                    </td>
+                    <td className='align-middle'>#{taskId}</td>
+                    <td className='align-middle'>{`${category}`}</td>
+                    <td className='align-middle'>
+                      <ButtonGroup>
+                        <Button
+                          variant='primary'
+                          onClick={() => toggleModal("view", index)}
+                        >
+                          More Details
+                        </Button>
+                        <div id={taskId}>
+                          <ViewDetailsModal
+                            show={buttonModalShow[index].view}
+                            task={task}
+                            onHide={() => toggleModal("view", index)}
+                            ariaLabelledBy={task.taskId}
+                          />
+                        </div>
+                        <Button
+                          variant='danger'
+                          onClick={() => toggleModal("report", index)}
+                        >
+                          Report Issue
+                        </Button>
+                        <ReportIssueModal
+                          show={buttonModalShow[index].report}
                           task={task}
-                          onHide={() => toggleModal("view", index)}
-                          ariaLabelledBy={task.taskId}
+                          onHide={() => toggleModal("report", index)}
                         />
-                      </div>
-                      <Button
-                        variant='danger'
-                        onClick={() => toggleModal("report", index)}
+                      </ButtonGroup>
+                    </td>
+                    <td className='align-middle'>
+                      <IconContext.Provider
+                        value={{ style: { fontSize: "30px" } }}
                       >
-                        Report Issue
-                      </Button>
-                      <ReportIssueModal
-                        show={buttonModalShow[index].report}
-                        task={task}
-                        onHide={() => toggleModal("report", index)}
-                      />
-                    </ButtonGroup>
-                  </td>
-                  <td className='align-middle'>
-                    <IconContext.Provider
-                      value={{ style: { fontSize: "30px" } }}
-                    >
-                      <div>
-                        {task.isCompleted ? (
-                          <TiTick />
-                        ) : task.volunteerId == null ? (
-                          <FaExclamationCircle />
-                        ) : (
-                          <GiEmptyHourglass />
-                        )}
-                      </div>
-                    </IconContext.Provider>
-                  </td>
-                </tr>
-              );
-            })}
-        </tbody>
-      </Table>
-    </Container>
-  );
+                        <div>
+                          {task.isCompleted ? (
+                            <TiTick />
+                          ) : task.volunteerId == null ? (
+                            <FaExclamationCircle />
+                          ) : (
+                            <GiEmptyHourglass />
+                          )}
+                        </div>
+                      </IconContext.Provider>
+                    </td>
+                  </tr>
+                );
+              })}
+          </tbody>
+        </Table>
+      </Container>
+    );
+  else return <Container/>
 };
 
 export default TaskRequestList;
